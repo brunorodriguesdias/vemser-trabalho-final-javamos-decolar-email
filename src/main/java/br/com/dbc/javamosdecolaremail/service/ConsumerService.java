8701a83c0f4ecfba5a0684a63f73fdcba24bda7a
Bruno <brunorodriguesdias@gmail.com>
2023-04-05 17:19:08 -0300

    wip

--- src/main/java/br/com/dbc/javamosdecolaremail/service/ConsumerService.java
+++ src/main/java/br/com/dbc/javamosdecolaremail/service/ConsumerService.java
@@ -1,5 +1,7 @@
 package br.com.dbc.javamosdecolaremail.service;
 
+import br.com.dbc.javamosdecolaremail.dto.EmailUsuarioDTO;
+import br.com.dbc.javamosdecolaremail.dto.VendaEmailDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +15,26 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-//@Service
-//@Slf4j
-//@RequiredArgsConstructor
-//public class ConsumidorService {
-//    private final EmailService emailService;
-//
-//    private final ObjectMapper objectMapper;
-//
-//    @KafkaListener(
-//            topics = "${kafka.topic}",
-//            groupId = "${kafka.group-id}",
-//            topicPartitions = {@TopicPartition(topic = "${kafka.topic}", partitions = {"0", "1"})},
-//            containerFactory = "listenerContainerFactory1"
-//    )
-//}
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ConsumerService {
+    private final EmailService emailService;
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(
+            topics = "${kafka.topic}",
+            groupId = "${kafka.group-id}",
+            topicPartitions = {@TopicPartition(topic = "${kafka.topic}", partitions = {"0", "1"})},
+            containerFactory = "listenerContainerFactory1"
+    )
+    public void sendEmail(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition) throws JsonProcessingException {
+        if(partition == 0) {
+            EmailUsuarioDTO emailUsuarioDTO = objectMapper.readValue(message, EmailUsuarioDTO.class);
+            emailService.sendEmail(emailUsuarioDTO);
+        } else {
+            VendaEmailDTO vendaEmailDTO = objectMapper.readValue(message, VendaEmailDTO.class);
+            emailService.sendEmail(vendaEmailDTO);
+        }
+    }
+}
