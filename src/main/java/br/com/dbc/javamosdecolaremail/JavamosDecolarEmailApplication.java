9d202169736243cca922d4dad6c59620e10e9f6b
Bruno <brunorodriguesdias@gmail.com>
2023-04-05 16:03:36 -0300

    criando projeto email

---
+++ src/main/java/br/com/dbc/javamosdecolaremail/JavamosDecolarEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbc.javamosdecolaremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamosDecolarEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamosDecolarEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbc/javamosdecolaremail/JavamosDecolarEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.javamosdecolaremail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamosDecolarEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
